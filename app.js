f1f1c15638277db1bff748d3e2ce2f437d8a88e1
gauravbhatt13 <gauravbhatt13@gmail.com>
2018-06-19 16:56:07 +0530

    'updated'

--- app.js
+++ app.js
@@ -5,80 +5,20 @@ const
     express = require('express'),
     bodyParser = require('body-parser'),
     request = require('request'),
-    app = express().use(bodyParser.json()),
-    alexa = require('ask-sdk'); // creates express http server
+    app = express().use(bodyParser.json())// creates express http server
+
+
+const facebook_token = "EAAV7ZCmOQmEABANF9YN3xKxVMgQa3JTIjC018c8ojUTCBFF2bT3cZCPheqrtGvuZBMFtTN0pQlOnWh0mmxBZAczMQAZCXA8HrYPYQFUvp95wZArKUva7rhJ8ODEWUCeTT1SdwSdqanwMBNUFqRP4dOt8MKaYLyK4CG2dZAYKdAEjQZDZD";
+
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Creates the endpoint for our webhook
-/*app.post('/webhook', (req, res) => {
-
-    let body = req.body;
-
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-
-});*/
-
-// Adds support for GET requests to our webhook
-/*
-app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "hello-webhook";
-
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
-});
-*/
 app.post('/speech-webhook', function (req, res) {
     // Get the city and date from the request
     let number = req.body.queryResult.parameters['number-integer'];
     res.json({ 'fulfillmentText': 'your ticket number ' + number + ' is under processing'});
     //res.sendStatus(200)
-});
-
-app.post('/alexa-webhook', function (req, res) {
-    const speechText = "testing alexa";
-    return alexa.handlerInput.responseBuilder
-        .speak("your ticket number is 120")
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
 });
 
 app.post('/webhook', function (req, res) {
@@ -87,20 +27,31 @@ app.post('/webhook', function (req, res) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            handleMessage(sender, event.message);
         }
     }
     res.sendStatus(200)
 });
 
-const token = "EAAV7ZCmOQmEABANF9YN3xKxVMgQa3JTIjC018c8ojUTCBFF2bT3cZCPheqrtGvuZBMFtTN0pQlOnWh0mmxBZAczMQAZCXA8HrYPYQFUvp95wZArKUva7rhJ8ODEWUCeTT1SdwSdqanwMBNUFqRP4dOt8MKaYLyK4CG2dZAYKdAEjQZDZD";
+function firstEntity(nlp, name) {
+    return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
+}
+
+function handleMessage(sender, message) {
+    // check greeting is here and is confident
+    const greeting = firstEntity(message.nlp, 'greetings');
+    if (greeting && greeting.confidence > 0.8) {
+        sendTextMessage(sender, 'Hi there!');
+    } else {
+        sendTextMessage(sender, "Text received, echo: " + message.text.substring(0, 200))
+    }
+}
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
+        qs: {access_token:facebook_token},
         method: 'POST',
         json: {
             recipient: {id:sender},
