a01a1729dab6737fa24adc8f04673406d0ae6bec
gauravbhatt13 <gauravbhatt13@gmail.com>
2018-06-19 18:07:03 +0530

    'updated'

--- app.js
+++ app.js
@@ -9,15 +9,17 @@ const
 
 
 //const facebook_token = "EAAV7ZCmOQmEABANF9YN3xKxVMgQa3JTIjC018c8ojUTCBFF2bT3cZCPheqrtGvuZBMFtTN0pQlOnWh0mmxBZAczMQAZCXA8HrYPYQFUvp95wZArKUva7rhJ8ODEWUCeTT1SdwSdqanwMBNUFqRP4dOt8MKaYLyK4CG2dZAYKdAEjQZDZD";
-const facebook_token = "EAADaw4BX5VsBAFNYwZA9uaYFRa5Byfz07NnXvS3DNxDMuHQ1DETgKueGyLNiUWUUoOELgwEVtxs7hcuz5H8YhrbEJQLceZAN98otesj8PsW5ZB85pvV5nqEJnJQuNex8tXZBYGNWS5mvLrYx41I7BQFXOHFImuuuOeA77VKrsAZDZD";
+const facebook_token = "EAAG3CBTuXN0BAJuZAYaui52SCsRuUoBX47cXjU644hZA3dL2ZAnNdLvBgfr9WlWOI8wCHh006nwglr5yDQZC7u9EDkycKqkOAbEj0J7ohF8CX4Sglq6fRFkU3ZChR8MypR5TNsmeOijTNaZCo709jnnRDEWgI4IzAMqqcyAEZBXUgZDZD";
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () = > console.log('webhook is listening')
+)
+;
 
 app.post('/speech-webhook', function (req, res) {
     // Get the city and date from the request
     let number = req.body.queryResult.parameters['number-integer'];
-    res.json({ 'fulfillmentText': 'your ticket number ' + number + ' is under processing'});
+    res.json({'fulfillmentText': 'your ticket number ' + number + ' is under processing'});
     //res.sendStatus(200)
 });
 
@@ -39,8 +41,12 @@ app.get('/webhook', function (req, res) {
             // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
-    };
-    /*let messaging_events = req.body.entry[0].messaging
+    }
+    ;
+});
+
+app.post('/webhook', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
     console.log('message received with events : ' + messaging_events.length);
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
@@ -49,7 +55,7 @@ app.get('/webhook', function (req, res) {
         if (event.message && event.message.text) {
             handleMessage(sender, event.message, res);
         }
-    }*/
+    }
 });
 
 function firstEntity(nlp, name) {
@@ -67,16 +73,16 @@ function handleMessage(sender, message, res) {
 }
 
 function sendTextMessage(sender, text, res) {
-    let messageData = { text:text }
+    let messageData = {text: text}
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:facebook_token},
+        qs: {access_token: facebook_token},
         method: 'POST',
         json: {
-            recipient: {id:sender},
+            recipient: {id: sender},
             message: messageData,
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
