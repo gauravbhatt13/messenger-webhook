2171163f68af94d51483b52914d6ab4929b9f4db
gauravbhatt13 <gauravbhatt13@gmail.com>
2018-06-20 04:35:16 +0530

    'updated'

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const
 const facebook_token = "EAAG3CBTuXN0BAJuZAYaui52SCsRuUoBX47cXjU644hZA3dL2ZAnNdLvBgfr9WlWOI8wCHh006nwglr5yDQZC7u9EDkycKqkOAbEj0J7ohF8CX4Sglq6fRFkU3ZChR8MypR5TNsmeOijTNaZCo709jnnRDEWgI4IzAMqqcyAEZBXUgZDZD";
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () = > console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.post('/speech-webhook', function (req, res) {
     // Get the city and date from the request
     let number = req.body.queryResult.parameters['number-integer'];
-    res.json({'fulfillmentText': 'your ticket number ' + number + ' is under processing'});
+    res.json({ 'fulfillmentText': 'your ticket number ' + number + ' is under processing'});
     //res.sendStatus(200)
 });
 
@@ -39,12 +39,8 @@ app.get('/webhook', function (req, res) {
             // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
-    }
-    ;
-});
-
-app.post('/webhook', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    };
+    /*let messaging_events = req.body.entry[0].messaging
     console.log('message received with events : ' + messaging_events.length);
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
@@ -53,8 +49,9 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
             handleMessage(sender, event.message, res);
         }
-    }
+    }*/
 });
+
 
 function firstEntity(nlp, name) {
     return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
@@ -71,16 +68,16 @@ function handleMessage(sender, message, res) {
 }
 
 function sendTextMessage(sender, text, res) {
-    let messageData = {text: text}
+    let messageData = { text:text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: facebook_token},
+        qs: {access_token:facebook_token},
         method: 'POST',
         json: {
-            recipient: {id: sender},
+            recipient: {id:sender},
             message: messageData,
         }
-    }, function (error, response, body) {
+    }, function(error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
