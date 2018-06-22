4bb262466bf49d7c1d5e075438e335c649e80ded
gauravbhatt13 <gauravbhatt13@gmail.com>
2018-06-22 22:04:33 +0530

    'updated'

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.post('/speech-webhook', function (req, res) {
 });
 
 app.post('/alexa-webhook', function (req, res) {
-    console.log(util.inspect(req, false, null));
+    //console.log(util.inspect(req, false, null));
+    console.log('request type : ' + req.body.request.type);
     const responseBody = {
         'intent': {
             'name' : 'Ticket_query',
